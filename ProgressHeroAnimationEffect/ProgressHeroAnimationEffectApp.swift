9277153ed8bf9d693621b595cbf7214649051d37
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-10-17 19:07:07 +0300

    Initial Commit

---
+++ ProgressHeroAnimationEffect/ProgressHeroAnimationEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressHeroAnimationEffectApp.swift
+//  ProgressHeroAnimationEffect
+//
+//  Created by Yunus Emre Taşdemir on 17.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProgressHeroAnimationEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
